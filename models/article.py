35daf957c96da26aebcca8e9ac0edcac1be8623f
Anthony mwaura <antony.mwaura@student.moringaschool.comc0deanthony@anthony-HP-EliteBook-840-G5:~/f11$ git config --global user.email antony.mwaura@student.moringaschool.com>
2024-12-16 16:15:35 +0300

    add

--- models/article.py
+++ models/article.py
@@ -1,54 +1,104 @@
-from database.connection import Connection
-from models.author import Author  # Import Author class
-from models.magazine import Magazine  # Import Magazine class
+# Import the database
+from database.connection import get_db_connection
 
 class Article:
     def __init__(self, id, title, content, author_id, magazine_id):
-        if not isinstance(id, int):
-            raise ValueError("ID must be an integer.")
-        if not isinstance(title, str) or not (5 <= len(title) <= 50):
-            raise ValueError("Title must be a string between 5 and 50 characters.")
-        if not isinstance(content, str):
-            raise ValueError("Content must be a string.")
-        if not isinstance(author_id, int):
-            raise ValueError("Author ID must be an integer.")
-        if not isinstance(magazine_id, int):
-            raise ValueError("Magazine ID must be an integer.")
-        
-        self._id = id
-        self._title = title
-        self._content = content
-        self._author_id = author_id
-        self._magazine_id = magazine_id
+        self.id = id
+        self.title = title if title else "Untitled"
+        self.content = content
+        self.author_id = author_id
+        self.magazine_id = magazine_id
 
-    @property
-    def id(self):
-        return self._id
+    def __repr__(self):
+        return f'<Article {self.title}>'
+    
 
     @property
     def title(self):
         return self._title
-
+    
     @title.setter
-    def title(self, value):
-        raise AttributeError("Cannot change title after initialization.")
-
+    def title(self, title):
+        if hasattr(self, '_title'):
+            raise AttributeError("Cannot change title after it has been set")
+        if isinstance(title, str) and len(title):
+            self._title = title
+        else:
+            raise ValueError(
+                "Title must be a non-empty string"
+            )
+        
     @property
     def content(self):
         return self._content
-
+    
     @content.setter
-    def content(self, value):
-        raise AttributeError("Cannot change content after initialization.")
-
+    def content(self, content):
+        if isinstance(content, str) and len(content):
+            self._content = content
+        else:
+            raise ValueError(
+                "Content must be a non-empty string"
+            )
+        
     @property
+    def author_id(self):
+        return self._author_id
+    
+    @author_id.setter
+    def author_id(self, author_id):
+        from models.author import Author
+        if type(author_id) is int and Author.find_by_id(author_id):
+            self._author_id = author_id
+        else:
+            raise ValueError(
+                "Author ID must reference an author in the database")
+        
+    @property
+    def magazine_id(self):
+        return self._magazine_id
+    
+    @magazine_id.setter
+    def magazine_id(self, magazine_id):
+        from models.magazine import Magazine
+        if type(magazine_id) is int and Magazine.find_by_id(magazine_id):
+            self._magazine_id = magazine_id
+        else:
+            raise ValueError("Magazine ID must reference a magazine in the database")
     def author(self):
-        query = "SELECT * FROM authors WHERE id = ?;"
-        result = Connection.get_db_connection().execute(query, (self._author_id,)).fetchone()
-        return Author(result[0], result[1]) if result else None
+        from models.author import Author
+        conn = get_db_connection()
+        CURSOR = conn.cursor()
+        """retrieves and returns the author who wrote this article"""
+        sql = """
+            SELECT a.*
+            FROM authors a
+            INNER JOIN articles ar ON ar.author = a.id
+            WHERE ar.id = ?
+        """
+        CURSOR.execute(sql, (self.id))
+        author_data = CURSOR.fetchone()
 
-    @property
+        if author_data:
+            return Author(*author_data)
+        else:
+            return None
+
     def magazine(self):
-        query = "SELECT * FROM magazines WHERE id = ?;"
-        result = Connection.get_db_connection().execute(query, (self._magazine_id,)).fetchone()
-        return Magazine(result[0], result[1]) if result else None
+        from models.magazine import Magazine
+        conn = get_db_connection()
+        CURSOR = conn.cursor()
+        """retrieves and returns the magazine in which this article is published"""
+        sql = """
+            SELECT m.*
+            FROM magazines m
+            INNER JOIN articles ar ON ar.magazine = m.id
+            WHERE ar.id = ?
+        """
+        CURSOR.execute(sql, (self.id))
+        magazine_data = CURSOR.fetchone()
+
+        if magazine_data:
+            return Magazine(*magazine_data)
+        else:
+            return None
